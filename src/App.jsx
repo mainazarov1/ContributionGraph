c3c3856a7c7305132710eedfdb59427bfad2f689
mainazarov1 <mainazarov1@gmail.com>
2023-08-31 14:20:47 +0600

    feat: change main files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ContributionGraph from "./components/ContributionGraph/ContributionGraph"
+import ContributionSquare from "./components/ContributionGraph/ContributionSquare/ContributionSquare"
+
+function App() {
+
+  return (
+    <>
+      <ContributionGraph />
+      
+    </>
+  )
+}
+
+export default App
